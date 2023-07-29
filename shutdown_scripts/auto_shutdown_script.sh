25e1e43ad1a00ea0fa87a005b030c3e4ab880ee1
Dr. Usman Kayani <51001263+drrobotk@users.noreply.github.com>
2023-07-29 02:13:00 +0100

    Update auto_shutdown_script.sh

--- shutdown_scripts/auto_shutdown_script.sh
+++ shutdown_scripts/auto_shutdown_script.sh
@@ -1,109 +1,81 @@
 #!/bin/bash
 
-# This script is used to automatically shutdown a VM instance on GCP using a number of triggers.
-# Author: Dr. Usman Kayani.
-
-# Declare the variables for the triggers.
-_threshold=2.0 # Load average threshold.
-consecutive_checks=3 # Number of consecutive checks to check if VM is idle.
+# Load configuration from environment variables or default values
+_threshold=${LOAD_AVERAGE_THRESHOLD:-2.0}
+consecutive_checks=${CONSECUTIVE_CHECKS:-3}
 trigger_check_dict=(
     "day_of_week:true"
     "time_of_day:true"
     "load_average:false"
 ) 
-trigger_interval=30 # Trigger interval in minutes
+trigger_interval=${TRIGGER_INTERVAL:-30} # Trigger interval in minutes
 
 poweroff () {
     # Function to poweroff the VM, with the reason and date appended to the
-    # logfile.
+    # syslog.
 
     sleep 2
-    echo "Powering off VM in 60 seconds. Reason: $1" >> script.log
-    echo "Intiated at $(date)." >> script.log
-    cat script.log
+    logger -t auto_shutdown "Powering off VM in 60 seconds. Reason: $1"
+    logger -t auto_shutdown "Initiated at $(date)."
     sleep 60
-    sudo poweroff
+    sudo poweroff || logger -t auto_shutdown "Failed to power off VM"
 }
 
 day_of_week () {
-    # Function to check if day of week is outside working days and if so,
-    # poweroff VM.
-    
-    # Check if day is a weekend day.
     _dow="$(date +'%A')"
-    if [ $_dow = 'Saturday' ] || [ $_dow = 'Sunday' ]
+    if [[ $_dow = 'Saturday' ]] || [[ $_dow = 'Sunday' ]]
     then
         poweroff "Outside of working days (day of week: $_dow)."
     fi
 }
 
 time_of_day () {
-    # Function to check if time of day is outside of working hours and if so,
-    # poweroff VM.
-
-    # Check if hour is between 10pm and 6am.
     _hour="$(date +'%H')"
-    if [ $_hour -ge 22 ] || [ $_hour -le 6 ]
+    if [[ $_hour -ge 22 ]] || [[ $_hour -le 6 ]]
     then
         poweroff "Outside of working hours (hour: $_hour)."
     fi
 }
 
 load_average () {
-    # Function to check if load average is above threshold and if so, poweroff
-    # VM.
-
     for (( i=1; i<=$consecutive_checks; i++ ))
     do
-        # Get the 15-minute load average and check if it is above the threshold
-        # a given number of times in a row.
-        _load_average=$(uptime | sed -e 's/.*load averages: //g' | awk '{ print $3 }')
-        res=$(echo $_load_average'<'$_threshold | bc -l)
-
-        if [ $res == 1 ]
+        _load_average=$(uptime | awk '{ print $10 }')
+        if (( $(echo "$_load_average < $_threshold" | bc -l) ))
         then
-            # If the VM is Idle after a check, append details to logfile.
-            echo "VM idling..." >> script.log
-            echo "Check number: $count." >> script.log
-            echo "Load average: $_load_average." >> script.log
-            echo "Date: $(date)." >> script.log
+            logger -t auto_shutdown "VM idling, load average: $_load_average"
         else
             break
         fi
 
-        # If afrer a number of consecutive checks, the VM is still idle, then
-        # poweroff.
-        if [ $i == $consecutive_checks ]
+        if (( i == consecutive_checks ))
         then
             poweroff "VM is idle after $i checks."
         fi
         sleep 60
-
     done
 }
 
 poweroff_triggers () {  
-    # Function to apply the triggers to poweroff the VM, where the triggers to
-    # check are specified in a dictionary.
-
-    # If the flag is true, then apply the triggers.
-    if [ $day_of_week_check == "true" ]
+    day_of_week_check=$(get_dict_val "day_of_week" "${trigger_check_dict[@]}")
+    time_of_day_check=$(get_dict_val "time_of_day" "${trigger_check_dict[@]}")
+    load_average_check=$(get_dict_val "load_average" "${trigger_check_dict[@]}")
+    
+    if [[ $day_of_week_check == "true" ]]
     then
         day_of_week
     fi
-    if [ $time_of_day_check == "true" ]
+    if [[ $time_of_day_check == "true" ]]
     then
         time_of_day
     fi
-    if [ $load_average_check == "true" ]
+    if [[ $load_average_check == "true" ]]
     then
         load_average
     fi
 }
 
 get_dict_val () {
-    # Function to get the value of a key from a dictionary.
-
 	local _key="$1"
 	shift
 	local _dict=("$@")
@@ -112,10 +84,8 @@ get_dict_val () {
 	do
 		key="${index%%:*}"
         
-        # If the key matches the one specified, return the value and break the
-        # for loop.
-		if [ $key == $_key ]
-		then
+        if [[ $key == $_key ]]
+        then
 			value="${index##*:}"
 			echo $value
 			break
@@ -125,17 +95,8 @@ get_dict_val () {
 }
 
 main () {
-    # Main function to start the logfile and apply the triggers after a
-    # specified time interval.
+    logger -t auto_shutdown "Started script at: $(date)."
 
-    echo "-------------------------------------------------------------------------------------------------------------------------" >> script.log
-    echo "Started script at: $(date)." >> script.log
-
-    # Get flags for each trigger.
-    day_of_week_check=$(get_dict_val "day_of_week" "${trigger_check_dict[@]}")
-    time_of_day_check=$(get_dict_val "time_of_day" "${trigger_check_dict[@]}")
-    load_average_check=$(get_dict_val "load_average" "${trigger_check_dict[@]}")
-    
     while true
     do
         poweroff_triggers
